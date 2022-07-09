2da6e91e4ab6c8210e3fbd0d643a09366ffb3781
Odeke12 <trevodex@gmail.com>
2022-07-09 12:26:47 +0300

    LCD screen shows parking status

--- parking1.ino
+++ parking1.ino
@@ -1,18 +1,26 @@
 #include<Servo.h>
+#include<Wire.h>
+#include <Adafruit_LiquidCrystal.h>
+
+#define LCD output
+Adafruit_LiquidCrystal lcd_1(0);
+
 int inches = 0;
 int inches2 = 0;
 int inches3 = 0;
 int number_of_slots=1;
 bool car;
 bool secondCar;
+bool carExiting,carLeft;
 int cm = 0;
 int secondDistance = 0;
+char message[50];
 
 Servo servo;
 
 long readUltrasonicDistance(int triggerPin, int echoPin)
 {
-  pinMode(triggerPin, OUTPUT); 
+  pinMode(triggerPin, OUTPUT);
   digitalWrite(triggerPin, LOW);
   delayMicroseconds(2);
   digitalWrite(triggerPin, HIGH);
@@ -31,11 +39,26 @@ void checkSlotStatus(){
   }
 }
 
+void vehicleExiting(){
+  if(inches2 <80 &&carExiting == true){
+    servo.write(90);
+  }else if(inches < 80 && carExiting == true){
+    carLeft = true;
+    servo.write(90);
+  }else if(carLeft == true && inches > 80){
+    servo.write(0);
+  }
+}
+
 void setup()
 {
+  lcd_1.begin(16, 2);
+
+  lcd_1.print("hello world");
+  
   Serial.begin(9600);
   servo.attach(10);
-  servo.write(0);
+  servo.write(0);  
 }
 
 void loop(){
@@ -44,12 +67,17 @@ void loop(){
   inches = (cm / 2.54);
    secondDistance = 0.01723 * readUltrasonicDistance(8, 8);
   inches2 = (secondDistance / 2.54);
+  vehicleExiting();
 if(number_of_slots > 0){
+  lcd_1.clear();
+  sprintf(message,"%d slot", number_of_slots);
+  lcd_1.print(message);
   if(inches < 80){
   servo.write(90);
    car = true;
   }else if(inches2 > 80 && secondCar == true){
    Serial.print("New position");
+   carExiting = true;
   servo.write(0);
   }else if(inches2 > 80 && car == true){
      Serial.println("Second car");
@@ -61,6 +89,8 @@ if(number_of_slots > 0){
   servo.write(0);
   }
 }else{
+lcd_1.clear();
+lcd_1.print("Parking full");
 servo.write(0);
 }
   checkSlotStatus();
