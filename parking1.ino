5f4601cfc9e5cc158d0207ded1b332c6d11fd32d
Odeke12 <trevodex@gmail.com>
2022-09-03 16:07:43 +0300

    Updating code after failing sensor

--- parking1.ino
+++ parking1.ino
@@ -1,52 +1,68 @@
+#include "NewPing.h"
 #include<Servo.h>
-#include<Wire.h>
-#include <Adafruit_LiquidCrystal.h>
 
-#define LCD output
-Adafruit_LiquidCrystal lcd_1(0);
+#define TRIGGER_PIN 3 // Trigger and Echo both on pin 9
+#define ECHO_PIN 3
 
-int inches = 0;
-int inches2 = 0;
-int inches3 = 0;
-int inches4 = 0;
-int number_of_slots=2;
+#define TRIGGER_PIN_1 5 // Trigger and Echo both on pin 3
+#define ECHO_PIN_1 5
+
+#define TRIGGER_PIN_2 12 // Trigger and Echo both on pin 12
+#define ECHO_PIN_2 12
+
+#define TRIGGER_PIN_3 11 // Trigger and Echo both on pin 11
+#define ECHO_PIN_3 11
+
+// Maximum distance we want to ping for (in centimeters).
+#define MAX_DISTANCE 400  
+// NewPing setup of pins and maximum distance.
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+NewPing sonar2(TRIGGER_PIN_1, ECHO_PIN_1, MAX_DISTANCE);
+NewPing sonar3(TRIGGER_PIN_2, ECHO_PIN_2, MAX_DISTANCE);
+NewPing sonar4(TRIGGER_PIN_3, ECHO_PIN_3, MAX_DISTANCE);
+
 bool car;
 bool secondCar;
 bool carEntering = false,carLeft,carLeaving;
-int cm = 0;
-int secondDistance = 0;
-char message[50];
-
+bool sensor1, sensor2;
+// Number of slots in the parking
+int number_of_slots=2;
+//Distances of the sensors
+int inches, inches2, inches3, inches4;
 Servo servo;
 
-long readUltrasonicDistance(int triggerPin, int echoPin)
-{
-  pinMode(triggerPin, OUTPUT);
-  digitalWrite(triggerPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(triggerPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(triggerPin, LOW);
-  pinMode(echoPin, INPUT);
-  return pulseIn(echoPin, HIGH);
-}
-
 void checkSlotStatus(){
- int distance = (0.01723 * readUltrasonicDistance(12, 12))/2.54;
- int distance2 = (0.01723 * readUltrasonicDistance(13, 13))/2.54; 
-  if((distance < 80 && number_of_slots > 0)){
+ int distance = sonar3.ping_cm();
+ int distance2 = sonar4.ping_cm(); 
+  if((distance < 5 && number_of_slots > 0)){
   number_of_slots --;
-  }else if((distance > 80 && number_of_slots < 1)){
+  }else if((distance > 5 && number_of_slots < 1)){
   number_of_slots ++;
-  }else if((distance2 < 80 && number_of_slots > 0)){
+  }else if((distance2 < 5 && number_of_slots > 0)){
    number_of_slots --;
-  }else if((distance2 > 80 && number_of_slots < 1)){
+  }else if((distance2 > 5 && number_of_slots < 1)){
    number_of_slots ++;
   }
 }
 
+void checkNewStatus(){
+ int distance = sonar3.ping_cm();
+ int distance2 = sonar4.ping_cm(); 
+  if(distance < 5){
+  sensor1 = true;
+  }else if(distance > 5){
+  sensor1 = false;
+  }
+  
+  if(distance2 < 5){
+   sensor2 = true;
+  }else if(distance2 > 5){
+   sensor2 = false;
+  }
+  }
+
 void vehicleExiting(){
-  if(inches2 <80 &&carEntering == true){
+  if(inches2 < 80 &&carEntering == true){
     Serial.print("Car leaving now");
     car = false;
     servo.write(90);
@@ -60,60 +76,78 @@ void vehicleExiting(){
   }
 }
 
-void setup()
-{
-  lcd_1.begin(16, 2);
-
-  lcd_1.print("hello world");
-  
+void setup() {
   Serial.begin(9600);
-  servo.attach(10);
-  servo.write(0);  
+   servo.attach(13);
+   servo.write(0);  
 }
 
-void loop(){
+int getDistance(int trigPin, int echoPin) // returns the distance (cm)
+{
+long duration, distance;
 
-  cm = 0.01723 * readUltrasonicDistance(7, 7);
-  inches = (cm / 2.54);
-   secondDistance = 0.01723 * readUltrasonicDistance(8, 8);
-  inches2 = (secondDistance / 2.54);
-    checkSlotStatus();
-  //vehicleExiting();
-if(number_of_slots > 0){
-  lcd_1.clear();
-  sprintf(message,"%d slot", number_of_slots);
-  lcd_1.print(message);
-  if(inches < 80){
-  servo.write(90);
-   car = true;
-  }else if(inches2 > 80 && secondCar == true){
+digitalWrite(trigPin, HIGH); // We send a 10us pulse
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+
+duration = pulseIn(echoPin, HIGH, 20000); // We wait for the echo to come back, with a timeout of 20ms, which corresponds approximately to 3m
+
+// pulseIn will only return 0 if it timed out. (or if echoPin was already to 1, but it should not happen)
+if(duration == 0) // If we timed out
+{
+pinMode(echoPin, OUTPUT); // Then we set echo pin to output mode
+digitalWrite(echoPin, LOW); // We send a LOW pulse to the echo pin
+delayMicroseconds(200);
+pinMode(echoPin, INPUT); // And finaly we come back to input mode
+}
+
+distance = (duration/2) / 29.1; // We calculate the distance (sound speed in air is aprox. 291m/s), /2 because of the pulse going and coming
+
+return distance; //We return the result. Here you can find a 0 if we timed out
+}
+
+void loop() {
+   inches = sonar.ping_cm();
+   inches2 = sonar2.ping_cm();
+   inches3 = sonar3.ping_cm();
+   inches4 = sonar4.ping_cm();
+   
+   Serial.print("Distance = ");
+  Serial.println(inches4);
+//  Serial.println(getDistance(11,11));
+  checkNewStatus();
+ if(sensor2 == false){
+   Serial.println("In the loop");
+  if(inches <= 5){
+    Serial.println("Car near");
+    servo.write(90);
+    car = true;
+   }else if(inches2 > 5 && secondCar == true){
    Serial.println("New position");
    carEntering = true;
   servo.write(0);
-  }else if(inches2 > 80 && car == true && carEntering == false){
+   }else if(inches2 > 5 && car == true && carEntering == false){
      Serial.println("Second car");
     servo.write(90);
-  }else if(inches2 < 80 && car == true && carEntering == false){
+  }else if(inches2 < 5 && car == true && carEntering == false){
     Serial.println("We are here");
    secondCar = true;
-  }else if(inches2 < 80){
+  }else if(inches2 < 5){
   Serial.println("Car at the gate");
   servo.write(90);
-  }else if(inches < 80){
+  }else if(inches < 5){
   servo.write(90);
   }
-}else{
-Serial.println("Else statement");
-lcd_1.clear();
-lcd_1.print("Parking full");
-  if(inches2 < 80){
+ }else{
+    if(inches2 < 5){
   carLeaving = true;
-  Serial.println("Car at the gate");
+  Serial.println("Car exiting");
   servo.write(90);
-  }else if(inches < 80 && carLeaving){
+  }else if(inches < 5 && carLeaving){
+   Serial.println("Car has left");
   servo.write(0);
   }
+  }
+  delay(500);
 }
 
-  delay(5); 
-}
